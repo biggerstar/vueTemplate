6b5726e707fd519d3f44820752902d7e1714644d
biggerstar <1458048116@qq.com>
2021-02-07 00:09:34 +0800

    vue Init Template

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <router-link to="/home">Home</router-link> |
+  </div>
+  <router-view/>
+</template>
+
+<style>
+ 
+</style>
---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	configureWepack:{
+		resolve:{
+			alias:{
+				"assets":"@/assets",
+				"components":"@/components",
+				"common":"@/common",
+				"network":"@/network",
+				"views":"@/views",
+			}
+		}
+	}
+
+}
